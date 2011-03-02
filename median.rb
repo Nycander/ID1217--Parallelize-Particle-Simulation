e3113417a7b685c114150699754a1472dbb57987
Martin Nycander <martin.nycander@gmail.com>
2011-03-02 21:42:02 +0100

    Added median script

---
+++ median.rb
@@ -0,0 +1,12 @@
+floats = []
+ARGV.each do | a |
+	if a.to_f == 0.0
+		$stderr.puts "FU! Invalid argument supplied. '#{a}' is not a float!"
+		exit
+	end
+	floats << a.to_f
+end
+
+floats.sort!
+
+puts floats[floats.length/2]
