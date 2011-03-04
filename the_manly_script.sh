417cc085c961a6ceeea7b7933b006bb1f0f9980a
co <coer@kth.se>
2011-03-04 11:27:54 +0000

    updated the manly script, Its working now?

--- the_manly_script.sh
+++ the_manly_script.sh
@@ -1,10 +1,9 @@
 #This script sure is manly.
 #Runs: mpi, openmp, pthreads and serial variants of the particle simulation.
-#export
 
 PROGRAMS="serial
-openmp
-pthreads"
+pthreads
+openmp"
 #mpi"
 
 test[0]=1
@@ -23,14 +22,29 @@ test[12]=4096
 test[13]=8192
 
 mkdir -p plot_dir
-rm -f plot_dir/*
+mkdir -p report/plots
+rm -f plot_dir/*.dat
+rm -f plot_dir/*.tmp
 
 for p in $PROGRAMS
 do
-    for i in `seq 0 1 13`
+    echo "The Frog King is brave ["$p"]"
+    outfile="plot_dir/"${p}"_out.dat"
+    datfile="plot_dir/"${p}".dat"
+    tmpfile="plot_dir/"${p}".tmp"
+    for i in `seq 10 10 1000`
     do
-        #echo $p $i
-        outfile="plot_dir/"${p}"_performance"
-        $p -n $test[i] | tail -n 1 | sed '/\d\.\d+/p' >> $outfile
+        echo "The Frog King is brave  as can be ["$i"]"
+        for i in `seq 1 1 5`
+        do
+            $p -n $test[i] | tail -n 1 | ruby sed.rb >> $tmpfile
+        done
+        cat $tmpfile | ruby median.rb >> $outfile
+        echo "" > $tmpfile
     done
+    echo "The Frog King is braver than me."
+    cat $outfile | ruby plotit.rb > $datfile
+    echo "The Frog King is braver as you can see."
+    echo "The Frog King is braver than you."
+    gnuplot "plot_dir/"$p".plt" > "report/plots/"$p".pdf"
 done
